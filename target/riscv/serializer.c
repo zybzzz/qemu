b83de137969fac7ed8154604a5d979cadfea2130
jiaxiaoyu <xyyy1420@gmail.com>
2023-12-05 16:31:23 +0800

    new memory layout: support new memory layout
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- target/riscv/serializer.c
+++ target/riscv/serializer.c
@@ -1,3 +1,6 @@
+#include "checkpoint/checkpoint.h"
+#include "cpu_bits.h"
+#include "qemu/error-report.h"
 #include "qemu/osdep.h"
 #include "qemu/log.h"
 #include "cpu.h"
@@ -15,87 +18,37 @@
 #include <zlib.h>
 #include "internals.h"
 #include "hw/intc/riscv_aclint.h"
-#include "checkpoint/checkpoint.h"
 #include "qapi/qapi-commands-machine.h"
+
+
+
+
 extern RISCVAclintMTimerState *my_riscv_mtimer;
-
-// 反转字符串的辅助函数
-static void reverse(char* str, int length) {
-    int start = 0;
-    int end = length - 1;
-    while (start < end) {
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        start++;
-        end--;
-    }
-}
-
-// 将整数转换为字符串的itoa函数
-static char* itoa(uint64_t num, char* str, int base) {
-    int i = 0;
-
-    // 处理0作为特殊情况
-    if (num == 0) {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
-    }
-
-    // 处理负数（仅适用于十进制）
-    if (base == 10) {
-    }
-
-    // 转换整数为字符串
-    while (num != 0) {
-        int rem = num % base;
-        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-        num = num / base;
-    }
-
-    // 不处理负数
-
-
-    str[i] = '\0'; // 字符串终止
-
-    // 反转字符串
-    reverse(str, i);
-
-    return str;
-}
-
-static int find_minlocation(void) {
-    uint64_t min = -1, idx;
-    for (int i = 0; i < SIMPOINT_IDX_MAX; i++){
-        if(serializer.simpoints[i] && serializer.simpoints[i] < min){
-            min = serializer.simpoints[i];
-            idx = i;
-        }
-    }
-    if(min != -1){
-        return idx+1;
-    }
-    return 0;
-}
 
 static void serializeRegs(void){
     CPUState *cs = qemu_get_cpu(0);
+
     for(int i = 0 ; i < 32; i++){
         cpu_physical_memory_write(INT_REG_CPT_ADDR + i*8, &cs->env_ptr->gpr[i], 8);
     }
+    info_report("Writting int registers to checkpoint memory");
+
     // F extertion
     for(int i = 0 ; i < 32; i++){
         cpu_physical_memory_write(FLOAT_REG_CPT_ADDR + i*8, &cs->env_ptr->fpr[i], 8);
     }
+    info_report("Writting float registers to checkpoint memory");
 
     cpu_physical_memory_write(PC_CPT_ADDR, &cs->env_ptr->pc, 8);
+
     // V extertion
     if(cs->env_ptr->vill){
         for(int i = 0; i < 32 * RV_VLEN_MAX / 64; i++){
             cpu_physical_memory_write(VECTOR_REGS_CPT_ADDR + i*8, &cs->env_ptr->vreg[i], 8);
-        }        
+        }
+        info_report("Writting vector registers to checkpoint memory");
     }
+
     // CSR registers
     for(int i = 0; i < CSR_TABLE_SIZE; i++){
         if(csr_ops[i].read != NULL){
@@ -104,14 +57,34 @@ static void serializeRegs(void){
             cpu_physical_memory_write(CSR_CPT_ADDR + i * 8, &val, 8);
         }
     }
+
     uint64_t val;
     val = CPT_MAGIC_BUMBER;
     cpu_physical_memory_write(BOOT_FLAGS, &val, 8);
-    uint64_t mtimecmp = riscv_aclint_mtimer_read(my_riscv_mtimer, my_riscv_mtimer->timecmp_base, 8);
-    cpu_physical_memory_write(BOOT_FLAGS+16, &mtimecmp, 8);
-    uint64_t mtime = riscv_aclint_mtimer_read(my_riscv_mtimer, my_riscv_mtimer->time_base, 8);
-    cpu_physical_memory_write(BOOT_FLAGS+24, &mtime, 8);
-    
+
+    uint64_t mstatus=cs->env_ptr->mstatus;
+    set_field(mstatus, MSTATUS_MPIE,get_field(mstatus, MSTATUS_MIE));
+    set_field(mstatus, MSTATUS_MIE,0);
+    set_field(mstatus, MSTATUS_MPP,cs->env_ptr->priv);
+    cpu_physical_memory_write(CSR_CPT_ADDR + 0x300 * 8, &val, 8);
+
+    uint64_t mepc=cs->env_ptr->mepc;
+    cpu_physical_memory_write(CSR_CPT_ADDR + 0x341 * 8, &val, 8);
+
+
+    uint32_t mtime_cmp;
+    uint64_t mtime_cmp_64;
+    cpu_physical_memory_read(CLINT_MMIO+CLINT_MTIMECMP, &mtime_cmp, 4);
+    mtime_cmp_64=(uint64_t)mtime_cmp;
+//    uint64_t mtimecmp = riscv_aclint_mtimer_read(my_riscv_mtimer, my_riscv_mtimer->timecmp_base, 8);
+    cpu_physical_memory_write(MTIME_CPT_ADDR, ()&mtime_cmp_64, 8);
+
+    uint32_t mtime;
+    uint64_t mtime_64;
+    cpu_physical_memory_read(CLINT_MMIO+CLINT_MTIME, &mtime, 4);
+    mtime_64=(uint64_t)mtime;
+//    uint64_t mtime = riscv_aclint_mtimer_read(my_riscv_mtimer, my_riscv_mtimer->time_base, 8);
+    cpu_physical_memory_write(MTIME_CMP_CPT_ADDR, &mtime_64, 8);
 }
 
 
@@ -120,26 +93,25 @@ static void serializePMem(uint64_t inst_count)
     uint32_t l;
     uint8_t buf[4096];
     MemoryInfo * info = qmp_query_memory_size_summary(NULL);
-    int min_locat_idx = find_minlocation();
     uint64_t addr = 0x80000000;
     uint64_t size = info->base_memory;
+    gzFile compressed_mem=NULL;
+    char *path=NULL;
+
     char filepath[200], str_num[100];
     if (checkpoint_state == SimpointCheckpointing) {
-        strcpy(filepath, pathmanger.outputPath);
-        strcat(filepath, "_");
-        memset(str_num, 0, 100);
-        strcat(filepath, itoa(serializer.simpoints[min_locat_idx-1], str_num, 10));
-        strcat(filepath, "_");
-        strcat(filepath, itoa(serializer.weights[min_locat_idx-1], str_num, 10));
-        strcat(filepath, "_.gz");
-        serializer.simpoints[min_locat_idx-1] = 0;
+        path=((GString*)(g_list_first(path_manager.checkpoint_path_list)->data))->str;
+        assert(g_mkdir_with_parents(dirname(path),0775)==0);
+        compressed_mem=gzopen(path);
     } else {
-        strcpy(filepath, pathmanger.outputPath);
+        strcpy(filepath, pathmanger.output_path);
+        assert(g_mkdir_with_parents(path_manager.output_path,0775)==0);
         strcat(filepath, "_");
         strcat(filepath, itoa(inst_count, str_num, 10));
         strcat(filepath, "_.gz");
+        compressed_mem=gzopen(filepath);
     }
-    gzFile compressed_mem = gzopen(filepath, "wb");
+
     if (!compressed_mem) {
         error_printf("filename %s can't open", filepath);
         return;
@@ -161,29 +133,36 @@ exit:
     gzclose(compressed_mem);
 }
 
+uint64_t get_next_instructions(){
+    GList* first_instr=g_list_first(serializer.cpt_instructions);
+    uint64_t instrs=0;
+    if (first_instr==NULL) {
+        return instrs;
+    }else{
+        return GPOINTER_TO_GINT(g_list_first(serializer.cpt_instructions)->data);
+    }
+    return instrs*serializer.cpt_interval;
+}
 
 static bool instrsCouldTakeCpt(uint64_t num_insts) {
-    int min_locat_idx = find_minlocation();
+    uint64_t limit_instructions=get_next_instructions()+100000;
+//    int min_locat_idx = find_minlocation();
     switch (checkpoint_state) {
         case SimpointCheckpointing:
-        if (!min_locat_idx) {
+        if (limit_instructions==0) {
             break;
         }else{
-            uint64_t next_point = serializer.simpoints[min_locat_idx - 1] * serializer.intervalSize + 100000;
-            if (num_insts >= next_point) {
-            info_report("Should take cpt now: %lu", num_insts);
-            return true;
+            if (num_insts >= limit_instructions) {
+                info_report("Should take cpt now: %lu", num_insts);
+                return true;
             } else if (num_insts % serializer.intervalSize == 0) {
-            info_report("First cpt @ %lu, now: %lu",
-            next_point, num_insts);
-            break;
+                info_report("First cpt @ %lu, now: %lu",
+                limit_instructions, num_insts);
+                break;
             }else{
-            break;
+                break;
             }
         }
-        case ManualOneShotCheckpointing:
-        return true;
-        case ManualUniformCheckpointing:
         case UniformCheckpointing:
         // if (num_insts >= nextUniformPoint) {
         //     info_report("Should take cpt now: %lu", num_insts);
@@ -196,7 +175,7 @@ static bool instrsCouldTakeCpt(uint64_t num_insts) {
         break;
     }
     return false;
-    }
+}
 // void notify_taken(uint64_t i) {
 //   info_report("Taking checkpoint @ instruction count %lu", i);
 //   if (checkpoint_state == SimpointCheckpointing) {
@@ -220,8 +199,7 @@ static bool instrsCouldTakeCpt(uint64_t num_insts) {
 
 static void serialize(uint64_t icount){
     serializeRegs();
-    
-    serializePMem(icount);  
+    serializePMem(icount); 
 }
 
 bool try_take_cpt(uint64_t icount) {
